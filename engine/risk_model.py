f27fde410d2c22d61b4ad95b6b296c0c85ceba47
badiaop1 <88845514+badiaop1@users.noreply.github.com>
2026-05-29 12:45:31 +0300

    Create risk_model.py

---
+++ engine/risk_model.py
@@ -0,0 +1,10 @@
+def evaluate_spread(spread):
+    """
+    Placeholder deterministic risk model.
+    """
+    return {
+        "max_profit": 0,
+        "max_loss": 0,
+        "breakeven": None,
+        "envelope_ok": False
+    }
